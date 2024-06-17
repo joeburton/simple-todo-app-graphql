3e45bed5bb98524b6adfae3ed1c908d2b9833ef6
Joe Burton <joeburton@gmail.com>
2024-06-17 16:42:39 +0100

    chore: updated fire func

--- api/fire.js
+++ api/fire.js
@@ -1,6 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function handler(res) {
-    res.status(200).json({ message: "Set this day on fire!" });
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello, world!" });
 }
-exports.default = handler;
--- src/fire.ts
+++
@@ -1,5 +0,0 @@
-import { Response } from "express";
-
-export default function handler(res: Response) {
-  res.status(200).json({ message: "Set this day on fire!" });
-}
